762cd86b0aa948e238aeaf4c65b76efca4ad228e
dsshv <ds.shvts@gmail.com>
2022-11-06 05:20:20 +0300

    work on task 5

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Homework 2
+
+A) Write the code where each node sends its computational power to the master node and the master calculates the total power of the cluster and the relative power of each node (master node including).
+
+B) If we assume that we want to fairly distribute a vector A[N] in nodes according to their computational power, write that piece of code next to the previous one that distributes the vector.
+
+C) Parallelize a for loop again depending on the relative computational power of each node in order to calculate pi (using Wallis product).
--- homeworks/homework_2/src/task_5.c
+++ homeworks/homework_2/src/task_5.c
@@ -4,11 +4,17 @@
 
 int main(int argc, char **argv) {
     int size, rank;
+    int i, j;
+    // for part A
     int proc_power;
-    int i;
     int total_capacity; 
     int *capacity;
-    double *relative_capacity;
+    float *relative_capacity;
+    // for part B
+    int part, n;
+    int *A;
+    int *splited_vector;
+    int last_point;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -33,17 +39,55 @@ int main(int argc, char **argv) {
             total_capacity  = total_capacity + capacity[i];
         }
 
-        for (i = 0; i<size; i++) {
-            relative_capacity[i] = (capacity[i] + 0.0) / total_capacity;
+        for (i = 0; i < size; i++) {
+            relative_capacity[i] = (float) capacity[i] / (float) total_capacity;
+            printf("\nNode [%d]: capacity = %d, revative power = %.5f", i, capacity[i], relative_capacity[i]);
         }
 
-        for (i = 0; i < size; i++) {
-            printf("\nNode [%d]: capacity = %d, revative power = %d", i, capacity[i], relative_capacity[i]);
-        }
-        printf("\nTotal capacity: %d\n", total_capacity);
+        printf("\nTotal capacity: %d", total_capacity);
     }
 
-    // Part
+    // Part B
+
+    if ( rank == 0 ){
+        printf("\n\nInput the size of vector n=");
+        scanf("%d",&n);
+        A = malloc ( n * sizeof(int));
+        for (i=0; i<n; i++) {
+            A[i] = rand() % 100;
+            printf("%5d", A[i]);
+        }
+
+        part = n * (relative_capacity[0]);
+        // printf("\npart = %d", part);
+        splited_vector = malloc(part*sizeof(int));
+        for (i = 0; i < part; i++) {
+            splited_vector[i] = A[i];
+        }
+        last_point = part;
+        for (i = 1; i < size; i++) {
+            //printf("\npart = %d", part);
+            part = n * (relative_capacity[i]);
+
+            printf("%2d --> %3d\n", i, part);
+            MPI_Send(&part, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&A[last_point], part, MPI_INT, i, 0, MPI_COMM_WORLD);
+            last_point = last_point + part;
+        }
+
+        part = n * relative_capacity[0];
+    }
+
+    if (rank > 0) {
+        MPI_Recv(&part, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // printf("rank[%d], part:%3d\n", rank, part);
+        splited_vector = malloc(part*sizeof(int));
+        MPI_Recv(splited_vector, part, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    printf("\nnode[%d]: part = %d\n", rank, part);
+    
+
     MPI_Finalize();
     return 0;
 }
